060bd6e8a10ca2fd25c0d72001d8e65d08abd853
Jacob Tye <jacob@eminenceaus.com>
2020-09-15 20:26:45 +1000

    Sort and Remove functions
    - Attempted both of these - segmentation errors
    - I am doing something wrong when swapping next and prev nodes around

--- Assignment 1/LinkedList.cpp
+++ Assignment 1/LinkedList.cpp
@@ -34,7 +34,14 @@ LinkedList::~LinkedList()
 // <-- ACCESSOR IMPLEMENTATION METHODS -->
 // -------------------------------------------------------------------
 
-// getHead() Initialisers
+// getMessage() Implementation
+// Returns full concatenated message store in the linked list
+LinkedList::value_type LinkedList::getMessage() const
+{
+    return message;
+}
+
+// getHead() Implementations
 // Handle both const and non-const function calls - giving the compiler the ability to figure out which is best to use
 // Non-const
 Node *LinkedList::getHead()
@@ -47,7 +54,7 @@ const Node *LinkedList::getHead() const
     return head;
 }
 
-// getTail() Initialisers
+// getTail() Implementations
 // Handle both const and non-const function calls - giving the compiler the ability to figure out which is best to use
 // Non-const
 Node *LinkedList::getTail()
@@ -60,7 +67,7 @@ const Node *LinkedList::getTail() const
     return tail;
 }
 
-// getCurrent() Initialisers
+// getCurrent() Implementations
 // Handle both const and non-const function calls - giving the compiler the ability to figure out which is best to use
 // Non-const
 Node *LinkedList::getCurrent()
@@ -74,24 +81,77 @@ const Node *LinkedList::getCurrent() const
 }
 
 // -------------------------------------------------------------------
+// <-- OPERATOR OVERLOAD METHODS -->
+// -------------------------------------------------------------------
+
+// Overloads += operator
+LinkedList &LinkedList::operator+=(const LinkedList &ls)
+{
+    add(ls.getMessage());
+    return *this;
+}
+
+// Overloads the operator '<<'
+std::ostream &operator<<(std::ostream &os, const LinkedList &ls)
+{
+    os << ls.getMessage();
+    return os;
+}
+
+// -------------------------------------------------------------------
 // <-- MUTATOR IMPLEMENTATION METHODS -->
 // -------------------------------------------------------------------
 
+// setMessage() Implementation
+// Sets message variable to &inputMessage
+void LinkedList::updateMessage()
+{
+    // Set temp variable to store the contents of each node - set as an empty string to start with
+    value_type newMessage = "";
+
+    // Reset current to head node for this counter
+    setCurrent(head);
+
+    // Continue this loop while the current node pointer does not equal the tail node pointer
+    while (current != tail)
+    {
+        // Build the new message from the data withing each node - make sure to include spaces between words
+        newMessage += " " + current->getData();
+        setCurrent(current->getNext());
+    }
+
+    // Grab the data from tail node for our last word
+    newMessage += " " + tail->getData();
+
+    // Give linked list member message the value of newMessage - updating it
+    message = newMessage;
+}
+
+// setHead() Implementation
+// Sets head variable to *headNode
 void LinkedList::setHead(Node *headNode)
 {
     head = headNode;
+    head->setPrevious(NULL);
 }
 
+// setTail() Implementation
+// Sets tail variable to *tailNode
 void LinkedList::setTail(Node *tailNode)
 {
     tail = tailNode;
+    tail->setNext(NULL);
 }
 
+// setCurrent() Implementation
+// Sets current variable to *currentNode
 void LinkedList::setCurrent(Node *currentNode)
 {
     current = currentNode;
 }
 
+// add() Implementation
+// Adds a full message to the linked list by breaking the message down into individual words and making them nodes
 void LinkedList::add(const value_type &message)
 {
     // Using stringstream to separate the words in the message added
@@ -103,10 +163,6 @@ void LinkedList::add(const value_type &message)
     // While looping through the message received
     while (messageReceived >> word)
     {
-
-        // Printing message (temporary debugging)
-        cout << word + " ";
-
         // Check if the head is NULL
         if (head == NULL)
         {
@@ -118,26 +174,89 @@ void LinkedList::add(const value_type &message)
         else
         {
             // If head is NOT NULL set the next node (for the current node) to the new node with the current word
-            current->setNext(new Node(word));
-            // AND ONLY THEN do you set the NEW current Node to the next node in the previous current node
-            setCurrent(current->getNext());
+            Node *newNode = new Node(word); // Declare a temp variable newNode
+            current->setNext(newNode);      // Set the current nextNode to the newNode
+            newNode->setPrevious(current);  // Set the newNode prevNode to the currentNode
+            setCurrent(newNode);            // Finally rewrite the current node as the newNode
         }
         // In this instance, the tail will always be the current word because the loop will finish on the final word (i.e: final node)
         setTail(current);
     }
 
-    // Some debug statements to make sure the linked list is roughly working
-    cout << endl
-         << "Head: " << endl;
-    cout << head->getData() << endl;
-    cout << "Tail: " << endl;
-    cout << tail->getData() << endl;
+    // Whenever we make changes to the string, we will call update message to hold the new data within the private member 'message'
+    updateMessage();
 }
 
+// remove() Implementation
+// Returns full concatenated message store in the linked list
 void LinkedList::remove(const value_type &word)
 {
+    // Reset current to head for this counter
+    setCurrent(head);
+
+    // Continue this loop while the current node pointer does not equal the tail node pointer
+    while (current != tail)
+    {
+        // Check if the current node's word equals the word we are deleting
+        if (current->getData() == word)
+        {
+            cout << word << endl;
+            setCurrent(current->getNext());
+        }
+        else
+        {
+            // If current node's word does not match the word being searched just set the current node to the next in the linked
+            setCurrent(current->getNext());
+        }
+    }
+
+    if (current->getData() == word)
+    {
+        cout << word << endl;
+        setCurrent(current->getNext());
+    }
+
+    updateMessage();
 }
 
+// sort() Implementation
+// Returns full concatenated message store in the linked list
+void LinkedList::sort()
+{
+    // Reset current to head for this counter
+    setCurrent(head);
+
+    // Continue this loop while the current node pointer does not equal the tail node pointer
+    while (current != tail)
+    {
+        if ( current->getData() > current->getNext()->getData() ) {
+            // Change nodes around this ones position
+            current->getPrevious()->setNext(current->getNext());    // This node -1 next = this node +1
+            current->getNext()->setPrevious(current->getPrevious());    // This node +1 prev = this node -1
+            current->getNext()->setNext(current);   // This node +1 next = this node 
+            current->getNext()->getNext()->setPrevious(current);    // This node +2 prev = this node 
+
+            // Change this nodes position
+            // Next = +2
+            // Prev = +1
+            current->setNext(current->getNext()->getNext());
+            current->setPrevious(current->getNext());
+
+            // Set new node
+            setCurrent(current->getPrevious());
+        } 
+    }
+
+    updateMessage();
+}
+
+// -------------------------------------------------------------------
+// <-- QUERY IMPLEMENTATION METHOD -->
+// I called this a query method because it doesn't really mutate the data, nor does it access it. It kind of just counts it - or 'queries' it :)
+// -------------------------------------------------------------------
+
+// count() Implementation
+// Counts occurences of a parsed word
 int LinkedList::count(const value_type &word)
 {
     // Declaring and instantiating our counter
@@ -146,7 +265,7 @@ int LinkedList::count(const value_type &word)
     // Reset current to head for this counter
     setCurrent(head);
 
-    // Continue this loop while the current node pointer does not equal the tail node pointer - doWhile needs to be used instead
+    // Continue this loop while the current node pointer does not equal the tail node pointer
     while (current != tail)
     {
         // Check if the current node's word equals the word we are counting
@@ -161,15 +280,10 @@ int LinkedList::count(const value_type &word)
 
     // Finally because we don't include tail in the while loop as it is the indicator of the end of the loop, we need to make sure the word in tail node is not the one we are searching for
     if (tail->getData() == word)
-    {   
+    {
         // If the tail node's word matches the word we are counting. Add an extra number to the counter.
         i++;
     }
 
     return i;
-}
-
-void LinkedList::sort()
-{
-    
 }
--- Assignment 1/LinkedList.h
+++ Assignment 1/LinkedList.h
@@ -10,50 +10,83 @@
 #include <sstream>
 #include "Node.h"
 
-using namespace std; 
+using namespace std;
 
-class LinkedList {
-    public:
-        // TYPEDEF DECLARATION
-        typedef Node::value_type value_type;
+class LinkedList
+{
+public:
+    // TYPEDEF DECLARATION
+    typedef Node::value_type value_type;
 
-        // CONSTRUCTOR DECLARATION
-        LinkedList();
+    // CONSTRUCTOR DECLARATION
+    // Precondition:
+    // Postcondition: LinkedList is created and implemented in LinkedList.cpp.
+    LinkedList();
 
-        // DESTRUCTOR DECLARATION
-        ~LinkedList();
+    // DESTRUCTOR DECLARATION
+    // Precondition: LinkedList instance is deleted
+    // Postcondition: Handles memory allocations
+    ~LinkedList();
 
-        // MUTATOR METHODS DECLARATION
-        // Set the head node in the linked list
-        void setHead(Node* nextNode);
-        // Set the tail node in the linked list
-        void setTail(Node* previousNode);
-        // Set the current node in the linked list
-        void setCurrent(Node* currentNode);
-        // Add a node to the linked list - parsing as a reference allocation for data persistence
-        void add(const value_type& inputData);
-        // Remove all nodes that contain a certain input in the linked list - parsing as a reference allocation for data persistence
-        void remove(const value_type& inputData);
-        // Count all occurences of a word in the linked list - parsing as a reference allocation for data persistence
-        int count(const value_type& inputData);
-        // Sort the list into alphabetical order
-        void sort();
+    // MUTATOR METHODS DECLARATION
+    // Precondition: There are nodes within the linked list
+    // Postcondition: message is set to a concatenation of all nodes in the list.
+    void updateMessage();
+    // Precondition:
+    // Postcondition: message is set to &message value.
+    void setHead(Node *nextNode);
+    // Precondition:
+    // Postcondition: message is set to &message value.
+    void setTail(Node *previousNode);
+    // Precondition: 
+    // Postcondition: message is set to &message value.
+    void setCurrent(Node *currentNode);
+    // Precondition: 
+    // Postcondition: message is set to &message value.
+    void add(const value_type &inputData);
+    // Precondition:
+    // Postcondition: message is set to &message value.
+    void remove(const value_type &inputData);
+    // Precondition: 
+    // Postcondition: message is set to &message value.
+    int count(const value_type &inputData);
+    // Precondition: 
+    // Postcondition: message is set to &message value.
+    void sort();
 
-        // ACCESSOR METHODS DECLARATION
-        // Methods to get the head node in linked list (both const and non-const)
-        const Node* getHead() const;
-        Node* getHead();
-        // Methods to get the tail node in linked list (both const and non-const)
-        const Node* getTail() const;
-        Node* getTail();
-        // Methods to get the current node in linked list (both const and non-const)t
-        const Node* getCurrent() const;
-        Node* getCurrent();
-    private:
-        // MEMBERS DECLARATION
-        Node* head;     // Pointer to a Node object - the head node of the linked list
-        Node* tail;     // Pointer to a Node object - the tail node of the linked list
-        Node* current;      // Pointer to a Node object - current node of the linked list
+    // ACCESSOR METHODS DECLARATION
+    // Precondition: Point p has x and y values
+    // Postcondition: returns ostream of Point p defined as (x,y).
+    const Node *getHead() const;
+    Node *getHead();
+    // Precondition: Point p has x and y values
+    // Postcondition: returns ostream of Point p defined as (x,y).
+    const Node *getTail() const;
+    Node *getTail();
+    // Precondition: Point p has x and y values
+    // Postcondition: returns ostream of Point p defined as (x,y).
+    const Node *getCurrent() const;
+    Node *getCurrent();
+    // Precondition: Point p has x and y values
+    // Postcondition: returns ostream of Point p defined as (x,y).
+    value_type getMessage() const;
+
+    // OPERATOR OVERLOADER DECLARATION
+    // Precondition: this Point and p have x and y values
+    // Postcondition: adds the x and y values of p to this Point's x and y values
+    LinkedList &operator+=(const LinkedList &ls);
+
+private:
+    // MEMBERS DECLARATION
+    Node *head;    // Pointer to a Node object - the head node of the linked list
+    Node *tail;    // Pointer to a Node object - the tail node of the linked list
+    Node *current; // Pointer to a Node object - current node of the linked list
+    value_type message;
 };
+
+// OPERATOR OVERLOADER DECLARATION
+// Precondition: Point p has x and y values
+// Postcondition: returns ostream of Point p defined as (x,y).
+std::ostream &operator<<(std::ostream &os, const LinkedList &ls);
 
 #endif
